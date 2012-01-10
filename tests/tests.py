064588277771b841cc7176c463a4dfd9bbe060e0
Nate Kupp <nathan.kupp@gmail.com>
2012-01-10 15:20:17 -0600

    Updated tests so checkBase() now verifies a base implements a given function.

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,9 @@
 import numpy as np
 import ffx, pandas
+from ffx.core import *
+
+def similar(a, b, eps):
+    return sum(abs(a - b)) < eps
 
 class TestFFX:
     def setUp(self): 
@@ -12,43 +16,46 @@ class TestFFX:
         self.xtest         = self.xtest_pandas.as_matrix()
         self.ytest  = self.data.ix[51:100,2]
 
-    def similar(self, a, b):
-        return sum(abs(a - b)) < self.EPS
-
     # ----------------------------------------------------------------
     # Test bases
     # ----------------------------------------------------------------
-    def runBase(self, model, data):
-        return self.similar(model.simulate(self.xtrain), data)
+    def checkBase(self, model, fn):
+        return similar(model.simulate(self.xtrain), fn(self.xtrain[:,0]), self.EPS)
 
     def testSimpleBase(self):
-        assert self.runBase(ffx.core.SimpleBase(0,1), self.xtrain[:,0])
-        assert self.runBase(ffx.core.SimpleBase(0,2), self.xtrain[:,0]**2)
+        assert self.checkBase(SimpleBase(0,1), lambda x: x)
+        assert self.checkBase(SimpleBase(0,2), lambda x: x**2)
 
-    def testOperatorBase(self):
-        a = ffx.SimpleBase(0,1)
-        assert self.runBase(ffx.core.OperatorBase(a, ffx.OP_ABS),        np.abs(self.xtrain[:,0]))
-        assert self.runBase(ffx.core.OperatorBase(a, ffx.OP_MAX0),       np.clip(self.xtrain[:,0], 0.0, ffx.INF))
-        assert self.runBase(ffx.core.OperatorBase(a, ffx.OP_MIN0),       np.clip(self.xtrain[:,0], -ffx.INF, 0.0))
-        assert self.runBase(ffx.core.OperatorBase(a, ffx.OP_LOG10),      np.log10(self.xtrain[:,0]))
-        assert self.runBase(ffx.core.OperatorBase(a, ffx.OP_GTH, 0.5),   np.clip(0.5 - self.xtrain[:,0], 0.0, ffx.INF))
-        assert self.runBase(ffx.core.OperatorBase(a, ffx.OP_LTH, 0.5),   np.clip(self.xtrain[:,0] - 0.5, 0.0, ffx.INF))
+    def testOperatorBase(self):    
+        base_simple = SimpleBase(0,1)
+        base_abs    = OperatorBase(base_simple, OP_ABS)
+        base_max    = OperatorBase(base_simple, OP_MAX0)
+        base_min    = OperatorBase(base_simple, OP_MIN0)
+        base_log10  = OperatorBase(base_simple, OP_LOG10)
+        base_gth    = OperatorBase(base_simple, OP_GTH, 0.5)
+        base_lth    = OperatorBase(base_simple, OP_LTH, 0.5)
+        
+        assert self.checkBase(base_abs,   np.abs)
+        assert self.checkBase(base_max,   lambda x: np.clip(x, 0.0, INF))
+        assert self.checkBase(base_min,   lambda x: np.clip(x, -INF, 0.0))
+        assert self.checkBase(base_log10, np.log10)
+        assert self.checkBase(base_gth,   lambda x: np.clip(0.5 - x, 0.0, INF))
+        assert self.checkBase(base_lth,   lambda x: np.clip(x - 0.5, 0.0, INF))
 
     def testProductBase(self):
-        a = ffx.core.SimpleBase(0,1)
-        b = ffx.core.SimpleBase(0,1)
-        c = ffx.core.SimpleBase(0,2)
-        assert self.runBase(ffx.core.ProductBase(a,b), self.xtrain[:,0]**2)
-        assert self.runBase(ffx.core.ProductBase(a,c), self.xtrain[:,0]**3)
-
+        a = SimpleBase(0,1)
+        b = SimpleBase(0,1)
+        c = SimpleBase(0,2)
+        assert self.checkBase(ProductBase(a,b), lambda x: x**2)
+        assert self.checkBase(ProductBase(a,c), lambda x: x**3)
 
     # ----------------------------------------------------------------
     # Test constant model
     # ----------------------------------------------------------------
     def testConstantModel(self):
-        mu = self.xtrain[:,0].mean()
-        a  = ffx.core.ConstantModel(mu,0).simulate(self.xtrain)
-        assert self.runBase(ffx.core.ConstantModel(mu,0),np.repeat(mu, self.xtrain.shape[0]))
+        mu   = self.xtrain[:,0].mean()
+        data = np.repeat(mu, self.xtrain.shape[0])
+        assert similar(ConstantModel(mu,0).simulate(self.xtrain), data, self.EPS)
 
     # ----------------------------------------------------------------
     # Test FFX API
