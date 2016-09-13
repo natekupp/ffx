570f1b27211ef114057ba65918306e623bba0599
James McDermott <jamesmichaelmcdermott@gmail.com>
2016-09-13 21:12:53 +0100

    Added py 3.5 to list of supported languages in setup.py.

--- setup.py
+++ setup.py
@@ -23,5 +23,6 @@ setup(
         'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
     ],
 )
