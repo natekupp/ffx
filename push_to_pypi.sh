bf95c92d5a2f317505efedaec2f1426ba4c5e2bf
James McDermott <jamesmichaelmcdermott@gmail.com>
2017-11-21 18:00:08 +0000

    Update push_to_pypi to use twine.

--- push_to_pypi.sh
+++ push_to_pypi.sh
@@ -1,1 +1,3 @@
-python setup.py sdist bdist_egg register upload
+# they don't recommend using "setup.py register upload" anymore
+python setup.py sdist bdist_egg bdist_wheel # just build
+twine upload dist/* # pip install twine if needed.
