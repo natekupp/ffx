d3e5f25e173dc405c291082fd2cd1b1d30405a32
Palmesi Pietro <Pietro.Palmesi@ait.ac.at>
2019-05-08 13:34:09 +0200

    updated api.py to pep8 and added sklearn test

--- tests/test_sklearn_api.py
+++ tests/test_sklearn_api.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import ffx
+from sklearn.utils.estimator_checks import check_estimator
 
 # This creates a dataset of 2 predictors
 X = np.random.random((20, 2))
@@ -16,3 +17,6 @@ print("Prediction:", FFX.predict(test_X))
 print("Score:", FFX.score(test_X, test_y))
 print("Complexity:", FFX.complexity())
 print("Model:", FFX._model)
+
+# This tests the regressor using sklearn's test facilities
+check_estimator(MyFFXRegressor)
